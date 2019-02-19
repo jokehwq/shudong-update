19e359a1d5e503efa00bddb19400ddd7b4642ff8
jokehwq <623765056@qq.com>
2019-02-20 00:38:12 +0800

    目前人们对房屋租赁买卖的需要，而信息不够详细、全面而设计的。主要是能让有房者发布其信息，让有买卖及租赁房屋需求者能查看到自己需要的信息，这样能使得更多的房屋能发挥其使用价值，而所有人们能更方便更快捷的进行房屋租赁买卖。

--- src/main/java/com/imooc/config/ElasticSearchConfig.java
+++ src/main/java/com/imooc/config/ElasticSearchConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by 瓦力.
+ * Created by .
  */
 @Configuration
 public class ElasticSearchConfig {
